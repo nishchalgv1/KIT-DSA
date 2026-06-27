0ac183e777d14327fa624711d13d97853f872644
nishchalgv1 <nischal.15gupta@gmail.com>
2026-06-27 11:24:14 +0530

    LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,43 @@
+// import java.util.Scanner;
+
+public class LinkedList{
+    // static Scanner sc = new Scanner(System.in);
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+    static void printLL(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 = new Node(30);
+        Node n4 = new Node(40);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        Node head = n1;
+        System.out.println("Printing head's data:");
+        System.out.println(head.data); // printing head's data
+
+        // print n2 data using n1
+        System.out.println("n2 data using n1: ");
+        System.out.println(n1.next.data);
+
+        printLL(head);
+
+    }
+}
---
+++ linkedList.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// There are 2 ways to create Linked List
+// 1. Statically
+// 2. Dynamically 
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    // constructor
+    Node(int data){
+        this -> data = data;
+        next = NULL;
+    }
+};
+
+void printLL(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+    // print linked list twice
+    cout << "Print linked list twice: " << endl;
+    temp = head;
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+}
+
+int main(){
+    // statically
+    Node n1(10);
+    Node n2(20);
+    Node n3(30);
+    Node n4(40);
+    Node n5(50);
+
+    Node *head = &n1;
+
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n5;
+    n5.next = NULL;
+
+    // cout << "head's data: " << head -> data << endl;
+    // // using n1 print n2 data
+    // cout << "print n2 data using n1: " << n1.next ->data << endl;
+    // cout << "print n3 data using n2: " << n2.next -> data << endl;
+
+    printLL(head);
+}
