d7bf86488b2df7304415272fb5345c468f1b7cef
nishchalgv1 <nischal.15gupta@gmail.com>
2026-06-30 11:22:27 +0530

    Inbuilt Stack in Java

---
+++ Stack/Main.java
@@ -0,0 +1,31 @@
+package Stack;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args){
+        Stack<Integer> st = new Stack<>();
+
+        // insert some elements
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        
+        int top_elem = st.peek();
+        System.out.println("Top element is: " + top_elem);
+
+        int removed_elem = st.pop();
+        System.out.println("Removed elem is: " + removed_elem);
+
+        int size = st.size();
+        System.out.println("Size is: " + size);
+
+        System.out.println("Is Stack Empty ?: " + st.isEmpty());
+
+        while(st.size() > 0){
+            int popped = st.pop();
+            System.out.println("Removed elem is: " + popped);
+        }
+        System.out.println("Is stack empty now?: " + st.isEmpty());
+    }
+}
