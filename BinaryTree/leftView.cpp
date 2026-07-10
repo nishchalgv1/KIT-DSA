c7c294841f6c284b3b872879e6497b8fcb66a6af
nishchalgv1 <nischal.15gupta@gmail.com>
2026-07-10 11:37:57 +0530

    left and right view of BT

---
+++ BinaryTree/leftView.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        vector<int> output;
+        if(root == NULL) return output;
+        
+        queue<Node*> pendingNodes;
+        pendingNodes.push(root);
+        
+        while(!pendingNodes.empty()){
+            int size = pendingNodes.size();
+            
+            for(int i = 0; i < size; i++){
+                Node* front = pendingNodes.front();
+                pendingNodes.pop();
+                if(i == 0){
+                    output.push_back(front -> data);
+                }
+                
+                if(front -> left != NULL){
+                    pendingNodes.push(front -> left);
+                }
+                
+                if(front -> right != NULL){
+                    pendingNodes.push(front -> right);
+                }
+            }
+        }
+        return output;
+        
+        
+    }
+};
